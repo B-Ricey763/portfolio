acbe9cafc024ab762f439cb8d4c1a53427ae2b6a
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2024-03-11 15:14:35 -0400

    Got desk into the world

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import { CameraControls, } from "@react-three/drei";
+import { CameraControls, Sphere, } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { CuboidCollider, Physics } from "@react-three/rapier";
 import { Suspense } from "react";
 import "./App.css";
 import Book from "./Book";
 import ThickMagazine from "./ThickMagazine";
+import { Desk } from "./desk";
 
 
+// <Book>
+//   <ThickMagazine />
+// </Book>
 
 function App() {
   return (
     <>
       <Canvas camera={{ position: [0, 8, 8] }}>
         <Suspense>
-          <Physics debug colliders={"hull"} gravity={[0, -9.8, 0]}>
-            <Book>
-              <ThickMagazine />
-            </Book>
-            <CuboidCollider args={[20, 1, 20]} position={[0, -2, 0]} />
-          </Physics>
+          <Desk />
         </Suspense>
-
         <CameraControls minDistance={5} maxDistance={100} />
-        <ambientLight intensity={Math.PI / 2} />
+        <pointLight position={[0, 50, 20]} intensity={0} >
+          <Sphere />
+        </pointLight>
+        <rectAreaLight position={[0, 50, 20]} width={10} height={10} intensity={100} />
       </Canvas>
     </>
   );
