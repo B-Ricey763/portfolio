e7aa3eb851ec963c1239cecc69d9d240d47fc516
Bryce Hanna <brycehanna@live.com>
2024-03-26 11:34:18 -0400

    added some more assets

--- src/overlays/BombFactoryTycoon.tsx
+++ src/overlays/BombFactoryTycoon.tsx
@@ -1,10 +1,5 @@
 export default function BombFactoryTycoon() {
   return (
-    <div>
-      <h1>Bomb Factory Tycoon</h1>
-      <p>
-        Bomb Factory Tycoon was a game
-      </p>
-    </div>
+    <ImageOverlay
   )
 }
