05b8e4ca160e37a9e86469e6458a5f95177eeec0
B-Ricey763 <39998963+B-Ricey763@users.noreply.github.com>
2024-04-30 20:41:44 -0400

    Custom domain setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/",
+  //base: "/portfolio/", Since I'm using a custom domain, I don't need this
 });
